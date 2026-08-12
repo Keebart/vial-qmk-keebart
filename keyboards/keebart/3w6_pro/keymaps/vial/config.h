a22f73d6180c8780df69264adf9d97b20e3e3c4a
Alexei Torgashov <alexei.torgashov@yahoo.de>
2026-08-12 20:28:20 +0200

    add 3w6 Pro keyboard

---
+++ keyboards/keebart/3w6_pro/keymaps/vial/config.h
@@ -0,0 +1,13 @@
+// Copyright 2026 Keebart
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x79, 0x18, 0x8F, 0x78, 0xDE, 0xEE, 0x44, 0x64}
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 16
+#define VIALRGB_NO_DIRECT
+#define VIAL_COMBO_ENTRIES 64
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {4, 4}
